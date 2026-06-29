37644079f16f53fa724210d5d24dbda492f94688
Samir210-az <samir.akhundoff@gmail.com>
2026-06-29 23:47:36 +0400

    Restore real app at repo root for sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,4 @@
-// Bu fayl kohne service-worker-i sondurur ki, butun domeni "tutub saxlamasin"
-// ve digar alt-sehifelere (mes: /an-rus-dili-platform/) mane olmasin.
+// M-CHAT-R üçün sadə service worker (yalnız PWA quraşdırma şərtini ödəyir, şəbəkəni keşləmir)
 self.addEventListener('install', () => self.skipWaiting());
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    Promise.all([
-      caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),
-      self.registration.unregister()
-    ]).then(() => {
-      return self.clients.matchAll().then(clients => {
-        clients.forEach(client => client.navigate(client.url));
-      });
-    })
-  );
-});
-
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', () => {});
