52e238525c766a1b024e036ba0660348555f8426
Samir210-az <samir.akhundoff@gmail.com>
2026-06-29 23:47:39 +0400

    Remove obsolete mchat/sw.js after repo rename

--- mchat/sw.js
+++
@@ -1,4 +0,0 @@
-// M-CHAT-R üçün sadə service worker (yalnız PWA quraşdırma şərtini ödəyir, şəbəkəni keşləmir)
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', () => {});
